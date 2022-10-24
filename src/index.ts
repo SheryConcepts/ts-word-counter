2162f166d819e7aaaaac7ddfca35e4597dc8effd
Shery <chsheharyarahmed@gmail.com>
2022-10-25 01:29:14 +0500

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello")
+import inquirer from "inquirer";
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "paragraph",
+        message: "enter your paragraph",
+    }
+]).then(
+    (v) => {
+        let count = 0;
+        v.paragraph.split(" ").forEach((v: any) => count ++);
+        console.log(`There are ${count} words in your paragraph`);
+    }
+)
